3757c064df9e1b0bf866d992ca2fc508953d2d76
An Nguyen <anguyen3@rockets.utoledo.edu>
2020-02-09 17:42:45 -0500

    Update addTwoHugeNumbersLinkedList.cpp

--- addTwoHugeNumbersLinkedList.cpp
+++ addTwoHugeNumbersLinkedList.cpp
@@ -5,17 +5,14 @@
 //   T value;
 //   ListNode *next;
 // };
-
 /* Approach 
 - Reverse two original lists
 - Sum (a,b,carry) from the end of the list, save carry if sum > 9999 limit
 - Add sum result to new node of sum_list
 - Take care of case when one list size is greater than the other
 - Take care of case when carry goes further than original max size of list
-- Reverse the sum_list
 - Time O(n), space O(1) if not counting creation of the sum_list else O(n)
 */
-
 ListNode<int> * reverseLinkedList(ListNode<int> * head)
 {
     ListNode<int> * prev = NULL;
@@ -35,15 +32,16 @@ ListNode<int> * reverseLinkedList(ListNode<int> * head)
 ListNode<int> * addTwoHugeNumbers(ListNode<int> * a, ListNode<int> * b) {
     ListNode<int> * a_reverse = reverseLinkedList(a);
     ListNode<int> * b_reverse = reverseLinkedList(b);
-    ListNode<int> * sum_reverse = NULL;
+    ListNode<int> * sumNode = NULL;
     ListNode<int> * currNode = NULL;
-    int carry = 0;
+    int carry = 0, sum = 0;
+    int a_val = 0, b_val = 0;
 
-    while (a_reverse || b_reverse)
+    while (a_reverse || b_reverse || carry)
     {
-        int a = a_reverse ? a_reverse->value : 0;
-        int b = b_reverse ? b_reverse->value : 0;
-        int sum = a + b + carry;
+        a_val = a_reverse ? a_reverse->value : 0;
+        b_val = b_reverse ? b_reverse->value : 0;
+        sum = a_val + b_val + carry;
         
         carry = 0;
         if (sum > 9999)
@@ -54,26 +52,12 @@ ListNode<int> * addTwoHugeNumbers(ListNode<int> * a, ListNode<int> * b) {
 
         ListNode<int> * nextNode = new ListNode(sum);
 
-        if (!sum_reverse)
-        {
-            sum_reverse = nextNode;
-            currNode = sum_reverse;
-        } else
-        {
-            currNode->next = nextNode;
-            currNode = nextNode;
-        }
+        nextNode->next = sumNode;
+        sumNode = nextNode;
 
         if (a_reverse) a_reverse = a_reverse->next;
         if (b_reverse) b_reverse = b_reverse->next;
     }
 
-    // When carry expands to further than the maximum size of original lists
-    if (carry)
-    {
-        ListNode<int> * nextNode = new ListNode(carry);
-        currNode->next = nextNode;
-    }
-
-    return reverseLinkedList(sum_reverse);
+    return sumNode;
 }
